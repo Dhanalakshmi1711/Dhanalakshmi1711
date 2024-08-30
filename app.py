124ae312b30ab45a9b6439b0b4f2c62029254fac
Dhanalakshmi1711 <106921364+Dhanalakshmi1711@users.noreply.github.com>
2024-08-30 12:41:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/api')
+def api():
+    return jsonify({"message": "Hello, API!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
