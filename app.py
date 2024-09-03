52971b6f2a613dab8f4a2e6039f43df7c6b8210e
Dhanalakshmi1711 <106921364+Dhanalakshmi1711@users.noreply.github.com>
2024-09-03 15:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, python!"
 
 @app.route('/api')
 def api():
-    return jsonify({"message": "Hello, API!"})
+    return jsonify({"message": "Hello, flask api!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=True)
